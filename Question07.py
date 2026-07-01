d0065b85d59923a65b1927f7b819a8c6fbef77b1
AtharvBhil <atharvbhil08@gmail.com>
2026-07-01 22:52:31 +0530

    Matrices and Operations.

---
+++ Question07.py
@@ -0,0 +1,12 @@
+import numpy as np
+A = np.array([[1,2,3],[4,5,6],[7,8,9]])
+B = np.array([[9,8,7],[6,5,4],[3,2,1]])
+# Matrix Addition
+print("Addition")
+print(A + B)
+# Matrix Multiplication
+print("\nMatrix Multiplication")
+print(A @ B)
+# Element-wise Multiplication
+print("\nElement-wise Multiplication")
+print(A * B)
