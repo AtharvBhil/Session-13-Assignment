3e8021fd3568fd19fa41344e06b028ca0b347f6a
AtharvBhil <atharvbhil08@gmail.com>
2026-07-01 22:26:57 +0530

    Question 4 Solved

---
+++ Question04.py
@@ -0,0 +1,11 @@
+import numpy as np
+arr1 = np.linspace(0, 5, 10)
+arr2 = np.linspace(-10, 10, 15)
+# Print arrays
+print(" 10 equally spaced numbers between 0 and 5.")
+print(arr1)
+print("Length:", len(arr1))
+
+print("\n15 equally spaced numbers between -10 and 10")
+print(arr2)
+print("Length:", len(arr2))
