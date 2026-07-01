c8ff02c6b649618c5180da3c94af08a3a6446ea2
AtharvBhil <atharvbhil08@gmail.com>
2026-07-01 22:47:52 +0530

    Vectors and Basic Operations

---
+++ Question06.py
@@ -0,0 +1,17 @@
+import numpy as np
+# Create vectors
+v1 = np.array([2, 4, 6, 8])
+v2 = np.array([1, 3, 5, 7])
+# Operations on vectors
+# Addition
+print("Addition")
+print(v1 + v2)
+# Subtraction
+print("\nSubtraction")
+print(v1 - v2)
+# Element-wise multiplication
+print("\nElement-wise Multiplication")
+print(v1 * v2)
+# Dot product
+print("\nDot Product")
+print(np.dot(v1, v2))
