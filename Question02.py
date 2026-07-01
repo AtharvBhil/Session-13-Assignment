299f0d05f443605fd5192cdee58ca3eca9f29088
AtharvBhil <atharvbhil08@gmail.com>
2026-07-01 21:58:49 +0530

    Array of zeros and ones

---
+++ Question02.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Create arrays
+arr1 = np.zeros(8)
+arr2 = np.ones((4, 4))
+arr3 = np.zeros((3, 3))
+
+# Print arrays
+print("1D Array of 8 Zeros")
+print(arr1)
+
+print("\n4x4 Array of Ones")
+print(arr2)
+
+print("\n3x3 Matrix of Zeros")
+print(arr3)
