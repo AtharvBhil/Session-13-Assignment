969e6be7107a741ec8a465e28161785ef461427f
AtharvBhil <atharvbhil08@gmail.com>
2026-07-01 22:17:13 +0530

    Question 3 Solved

---
+++ Question03.py
@@ -0,0 +1,14 @@
+import numpy as np
+# Different arrays using arange()
+arr1 = np.arange(0, 21, 1)
+arr2 = np.arange(10, 51, 2)
+arr3 = np.arange(5, 101, 5)
+# Array of numbers from 0 to 20
+print("0 to 20")
+print(arr1)
+# Array of even numbers from 10 to 50
+print("\nEven Numbers from 10 to 50")
+print(arr2)
+# Array of numbers from 5 to 100 with step 5
+print("\n5 to 100 with Step 5")
+print(arr3)
